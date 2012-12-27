ecf1b4739eaa24f7da318afb3deb723f99e91ae0
David Steele <dsteele@gmail.com>
2012-12-27 17:05:27 -0500

    01 Add skeleton for python replacement of detect-well-known-errors
    Contains the main section loop, and establishes the working directories
    kpr and bug extensions are renamed, to allow side-by-side evaluation against
      the original bash script.

---
+++ master-bin/detect_well_known_errors.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# Copyright 2013 David Steele (dsteele@gmail.com)
+#
+# This file is part of Piuparts
+#
+# Piuparts is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# Piuparts is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import ConfigParser
+import piupartslib
+import os
+import time
+
+CONFIG_FILE = "/etc/piuparts/piuparts.conf"
+KPR_DIRS = ( 'pass', 'bugged', 'affected', 'fail' )
+
+KPR_EXT = '.kpr'
+BUG_EXT = '.bug'
+
+class WKE_Config( piupartslib.conf.Config ):
+    """Configuration parameters for Well Known Errors"""
+
+    def __init__( self ):
+        self.section = 'global'
+
+        piupartslib.conf.Config.__init__( self, self.section,
+            {
+                "sections": "sid",
+                "master-directory": "/var/lib/piuparts/master/",
+            }, "" )
+
+def process_section( section, config ):
+    """ Update .bug and .kpr files for logs in this section """
+
+    sectiondir = os.path.join( config['master-directory'], section )
+    workdirs = [ os.path.join(sectiondir,x) for x in KPR_DIRS ]
+
+    if not os.access( sectiondir, os.F_OK ):
+        return
+
+    [os.mkdir(x) for x in workdirs if not os.path.exists(x)]
+
+def detect_well_known_errors( config ):
+
+    for section in config['sections'].split(" "):
+        print time.strftime( "%a %b %2d %H:%M:%S %Z %Y", time.localtime() )
+        print "%s:" % section
+
+        process_section( section, config )
+
+    print time.strftime( "%a %b %2d %H:%M:%S %Z %Y", time.localtime() )
+
+if __name__ == '__main__':
+
+    conf = WKE_Config()
+    conf.read( CONFIG_FILE )
+
+    detect_well_known_errors( conf )
